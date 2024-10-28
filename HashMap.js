53d2fbf86f0b38164b102658d8081b057d1f7e9f
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2024-10-28 18:05:12 +0100

    Added a resize method and eliminated console.logs

--- HashMap.js
+++ HashMap.js
@@ -3,19 +3,23 @@ class HashMap{
     constructor(bucketSize = 53){
         this.map = new Array(bucketSize);
         this.bucketSize = bucketSize;
+        this.size = 0;
+        this.loadFactor = 0.75;
     }
 
     hash(key){
         let hashCode = 0;
         const primeNumber = 31;
         for(let i = 0; i < key.length; i++){
-            hashCode = primeNumber * hashCode + key.charCodeAt(i);
-            hashCode = hashCode % this.bucketSize;
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.bucketSize;
         }
         return hashCode;
     }
 
     set(key, value){
+        if(this.size / this.bucketSize >= this.loadFactor){
+            this.resize();
+        }
         const index = this.hash(key);
         if(!this.map[index]){
             this.map[index] = [];
@@ -27,6 +31,22 @@ class HashMap{
             }
         }
         this.map[index].push([key, value]);
+        this.size++;
+    }
+
+    resize(){
+        const oldMap = this.map;
+        this.bucketSize *= 2;
+        this.map = new Array(this.bucketSize);
+        this.size = 0;
+
+        for(const bucket of oldMap){
+            if(bucket){
+                for(const[key, value] of bucket){
+                    this.set(key, value);
+                }
+            }
+        }
     }
 
     get(key){
@@ -119,42 +139,5 @@ class HashMap{
         return entriesArray;
     }
 }
-
-// Testing:
-const testHashMap = new HashMap()
-// Test 1: Hash method
-console.log("Hash for Luffy:", testHashMap.hash("Luffy"));
-console.log("Hash for Zoro:", testHashMap.hash("Zoro"));
-//Test 2: Set and get methods
-testHashMap.set("Luffy", "Captain");
-testHashMap.set("Zoro", "Swordmaster");
-testHashMap.set("Nami", "Navigator");
-console.log("Entries after set method:");
-console.log("Luffy:", testHashMap.get("Luffy"));
-console.log("Zoro:", testHashMap.get("Zoro"));
-console.log("Nami:", testHashMap.get("Nami"));
-// Test 3: Updating an entry
-testHashMap.set("Luffy", "King of Pirates");
-console.log("Updated Luffy:", testHashMap.get("Luffy"));
-//Test 4: Has and remove methods
-console.log("Luffy exists?", testHashMap.has("Luffy"));
-console.log("Sanji exists?", testHashMap.has("Sanji"));
-testHashMap.set("Sanji", "Cook");
-console.log("Removing Sanji:", testHashMap.remove("Sanji"));
-console.log("Sanji exists anymore?:", testHashMap.has("Sanji"));
-// Test 5: Length and clear methods
-console.log("Total keys in map:", testHashMap.length());
-testHashMap.clear();
-console.log("Total keys in map after clear:", testHashMap.length());
-console.log("Setting keys again...");
-testHashMap.set("Luffy", "Captain");
-testHashMap.set("Zoro", "Swordmaster");
-testHashMap.set("Nami", "Navigator");
-// Test 6: Keys, values and entries methods
-console.log("Keys array:", testHashMap.keys());
-console.log("Values array:", testHashMap.values());
-console.log("Entries array:", JSON.stringify(testHashMap.entries()));
-
-
 
 
